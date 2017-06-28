ddedd4ef35c8ff44f4cb0ac409e313ffc32c8f8c
conrad <conrad@localhost.localdomain>
2017-06-28 19:10:46 +1000

    very rough and messy implementation of chol()

--- include/bandicoot_bits/mul_gemm.hpp
+++ include/bandicoot_bits/mul_gemm.hpp
@@ -92,7 +92,36 @@ class gemm
     const double beta  = double(0)
     )
     {
-    coot_stop_runtime_error("gemm::apply(): unimplemented for eT = double");
+    coot_extra_debug_sigprint();
+    
+    // coot_debug_assert_blas_size(A,B);  // TODO: adapt this assert for size_t
+    
+    const clblasTranspose transA = (do_trans_A) ? clblasTrans : clblasNoTrans;
+    const clblasTranspose transB = (do_trans_B) ? clblasTrans : clblasNoTrans;
+    
+    const size_t M = size_t(C.n_rows);
+    const size_t N = size_t(C.n_cols);
+    const size_t K = (do_trans_A) ? size_t(A.n_rows) : size_t(A.n_cols);
+    
+    const size_t lda = (do_trans_A) ? K : M;
+    const size_t ldb = (do_trans_B) ? N : K;
+    const size_t ldc = size_t(C.n_rows);
+    
+    const double local_alpha = (use_alpha) ? alpha : float(1);
+    const double local_beta  = (use_beta)  ? beta  : float(0);
+    
+    cl_mem A_mem = A.get_dev_mem(false);
+    cl_mem B_mem = B.get_dev_mem(false);
+    cl_mem C_mem = C.get_dev_mem(false);
+    
+    cl_command_queue queue = coot_rt.get_cq();
+    
+    cl_int status = 0;
+    
+    status |= clblasDgemm(clblasColumnMajor, transA, transB, M, N, K, local_alpha, A_mem, 0, lda, B_mem, 0, ldb, local_beta, C_mem, 0, ldc, 1, &queue, 0, NULL, NULL);
+    status |= clFlush(queue);
+    
+    coot_check_cl_error(status, "gemm::apply(): eT = double");
     }
   
   
---
+++ tests/chol.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <armadillo>
+#include <bandicoot>
+
+int
+main()
+  {
+  std::cout << coot::coot_version::as_string() << std::endl;
+  
+  //coot::coot_rt.init();
+  coot::coot_rt.init("coot_config.txt", true);
+  //coot::coot_rt.init(true);
+  //coot::coot_rt.init(2,0);
+    
+  arma::dmat A(5,5, arma::fill::randu); A -= 0.5;
+  A.print("A:");
+  
+  arma::dmat X = A*A.t();
+  X.print("X:");
+  
+  arma::dmat R;
+  arma::chol(R,X);
+  R.print("R:");
+  
+  std::cout << std::endl;
+  std::cout << "----" << std::endl;
+  std::cout << std::endl;
+  
+  coot::dmat AA(A);
+  
+  AA.print("AA:");
+  
+  coot::dmat XX = AA*AA.t();
+  XX.print("XX:");
+  
+  coot::dmat RR;
+  coot::chol(RR,XX);
+  RR.print("RR:");
+  
+  
+  return 0;
+  }
